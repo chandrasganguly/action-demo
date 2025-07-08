ede54319c149744750f6cf2028af35f0ae945226
chandrasganguly <83635706+chandrasganguly@users.noreply.github.com>
2025-07-08 20:18:12 +0900

    Action Demo
    Action Demo

---
+++ src/main/java/com/example/action_demo/ActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.action_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/action_demo/UserController.java
@@ -0,0 +1,21 @@
+package com.example.action_demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    
+
+    // Get all users
+    @GetMapping
+    public String getAllUsers() {
+        return "hello";
+    }
+
+    
+}
